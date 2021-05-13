f9535a8cc26cd60f9d3a7c900c54facbd5806022
Kiledjia <56278781+Kiledjia@users.noreply.github.com>
2021-05-12 19:02:32 -0700

    Update mytoolkit.js

--- mytoolkit.js
+++ mytoolkit.js
@@ -5,17 +5,24 @@ import {SVG} from './svg.min.js';
 var MyToolkit = (function() {
     var Button = function(){
         var draw = SVG().addTo('body').size('100%','100%');
-        var rect = draw.rect(100,50).fill('red')
-        var clickEvent = null
+        
+        var rect = draw.rect(100,50).fill('#6699cc')
 
+        var text = draw.text('Button')
+        .font({size: 20, family: 'Helvetica'});
+        
+        var clickEvent = null
         rect.mouseover(function(){
-            this.fill({ color: 'blue'})
+            this.fill({ color: '#808080'})
+            console.log("mouseover Change state")
         })
         rect.mouseout(function(){
-            this.fill({ color: 'red'})
+            this.fill({ color: '#6699cc'})
+            console.log("mouseout Change state")
         })
         rect.mouseup(function(){
-            this.fill({ color: 'red'})
+            this.fill({ color: '#6699cc'})
+            console.log("mouseup Change state")
         })
         rect.click(function(event){
             this.fill({ color: 'pink'})
@@ -25,12 +32,15 @@ var MyToolkit = (function() {
         return {
             move: function(x, y) {
                 rect.move(x, y);
+                text.move(x+21,y+12);
             },
             onclick: function(eventHandler){
                 clickEvent = eventHandler
             }
         }
     }
+    
+    
 return {Button}
 }());
 
