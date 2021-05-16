14af33de7567d689c9905d092d4486851dadaee2
Kiledjia <56278781+Kiledjia@users.noreply.github.com>
2021-05-16 15:35:18 -0700

    Update mytoolkit.js

--- mytoolkit.js
+++ mytoolkit.js
@@ -190,7 +190,7 @@ var MyToolkit = (function() {
         }
     }
     var TextBox = function(){
-        var draw = SVG().addTo('body').size('100%','100%');
+        var draw = SVG().addTo('body').size('100%','100%').height(80);
         var frame = draw.group();
         frame.rect(400,50).stroke("#6699cc").fill("White")
         frame.click(function(event){
@@ -212,16 +212,8 @@ var MyToolkit = (function() {
             console.log(event)
             }  
         })
-        // while(text.length()<75){
-            frame.move(10,10)
-        // }
-        // if(te > 75){
-        //     frame.move(0,0)
-        // }
-        // var circle  = draw.circle(15).fill('none').stroke({ width: 2, color: '#808080' })     
+        frame.move(10,10)
 
-        // var label = draw.text('Radio Label')
-        // .font({size: 16, family: 'Helvetica'});
 
         var clickEvent = null
         var mouseoverEvent = null
@@ -284,7 +276,103 @@ var MyToolkit = (function() {
             }
         }
     }
-return {Button,CheckBox,RadioButton,TextBox}
+    var ScrollBar = function(){
+        var draw = SVG().addTo('body').size('100%','100%');
+        var frame = draw.group();
+        frame.rect(20,300).fill('none').stroke({ width: 2, color: '#808080' })
+        frame.click(function(event){
+            console.log("Window")
+            console.log(event)
+        })
+        var thumb = draw.rect(15, 50).fill('#6699cc')
+        thumb.radius(10)
+        
+        var up = draw.text('▲')
+        .font({size: 17, family: 'Helvetica', weight: "bold"}).fill("#6699cc")
+        var down = draw.text('▼')
+        .font({size: 17, family: 'Helvetica', weight: "bold"}).fill("#6699cc")
+        
+        
+
+
+        var clickEvent = null
+        var mouseoverEvent = null
+        var mouseoutEvent = null
+        var mouseupEvent = null
+
+        thumb.mouseover(function(){
+            this.fill({ color: '#517AA3'})
+            if(mouseoverEvent != null)
+                mouseoverEvent(event)
+        })
+        thumb.mouseout(function(){
+            this.fill({ color: '#6699cc'})
+            if(mouseoutEvent != null)
+            mouseoutEvent(event)
+        })
+        thumb.mouseup(function(){
+            this.fill({ color: '#6699cc'})
+            if(mouseupEvent != null)
+                mouseupEvent(event)
+        })
+        thumb.click(function(event){
+            this.fill({ color: '#334C66'})
+            // thumb.move(frame.x()+2.5,150)
+                if(clickEvent != null)
+                    clickEvent(event)
+        })
+        up.click(function(event){
+            if(thumb.y()-30>frame.y()){
+                thumb.move(thumb.x(),thumb.y()-10)
+            }
+                if(clickEvent != null)
+                    clickEvent(event)
+                    console.log("scroll thumb has moved up")
+        })
+        down.click(function(event){
+            if(thumb.y()<frame.height()-80){
+                thumb.move(thumb.x(),thumb.y()+10)
+            }
+                if(clickEvent != null)
+                    clickEvent(event)
+                    console.log("scroll thumb has moved down")
+        })
+        thumb.mousedown(function(){
+            SVG.on(thumb,'mousemove',(event)=>{
+                if(thumb.y()<frame.height()-80){
+                    thumb.move(thumb.x(),event.offsetY)
+                    console.log(event)
+                    console.log(event.offsetY)                
+                }
+            })
+        })
+        return {
+            move: function(x, y) {
+                frame.move(x, y);
+                thumb.move(frame.x()+2.5,frame.y()+25)
+                up.move(frame.x()+1.5, frame.y()+1.5);
+            },
+            setHeight: function(height){
+                frame.height(height)
+                draw.height(height);
+                up.move(frame.x()+1.5, frame.y()+1.5);
+                down.move(frame.x()+1.5, frame.height()-20)
+            },
+            onclick: function(eventHandler){
+                clickEvent = eventHandler
+            },
+            onmouseout: function(eventHandler){
+                mouseoverEvent = eventHandler
+            },
+            onmouseup: function(eventHandler){
+                mouseoutEvent = eventHandler
+            },
+            onmouseover: function(eventHandler){
+                mouseupEvent = eventHandler
+            }
+        }
+    }
+return {Button,CheckBox,RadioButton,TextBox,ScrollBar}
 }());
 
 export{MyToolkit}
